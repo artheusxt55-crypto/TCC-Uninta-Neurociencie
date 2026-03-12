a56d13777d0e0dcd299fc737901c3cf4d69c42b0
Githubtheus <artheusxt55@gmail.com>
2026-03-12 17:27:00 -0300

    Update vite.config.ts

--- aura/vite.config.ts
+++ aura/vite.config.ts
@@ -1,8 +1,22 @@
+// aura/vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Isso força o Vite a usar caminhos relativos
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        // Renomeie a saída do chat aqui para não ser "index"
+        aura_chat: './index.html' 
+      },
+      output: {
+        // Isso vai gerar um arquivo chamado aura_chat.html no final
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
+    }
+  }
 })
