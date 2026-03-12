15b006df6c8d317cd601e35151f307f5ff406ee1
Githubtheus <artheusxt55@gmail.com>
2026-03-12 17:42:32 -0300

    Delete aura directory

--- aura/src/pages/Index.tsx
+++
@@ -1,9 +0,0 @@
-import { ChatInterface } from "../components/ChatInterface";
-
-export default function Index() {
-  return (
-    <div className="min-h-screen bg-background">
-      <ChatInterface />
-    </div>
-  );
-}
