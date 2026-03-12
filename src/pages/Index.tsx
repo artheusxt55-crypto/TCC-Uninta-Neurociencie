6347c92bf30a6891fb4f71ea41724f09e967bff4
Githubtheus <artheusxt55@gmail.com>
2026-03-12 15:36:37 -0300

    Create Index.tsx

---
+++ src/pages/Index.tsx
@@ -0,0 +1,9 @@
+import ChatInterface from "../components/ChatInterface";
+
+export default function Index() {
+  return (
+    <div className="min-h-screen bg-background">
+      <ChatInterface />
+    </div>
+  );
+}
