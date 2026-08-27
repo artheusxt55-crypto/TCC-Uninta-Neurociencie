0687132ea291fda0c8125264ba792728fbbe40af
Githubtheus <artheusxt55@gmail.com>
2026-08-26 21:18:11 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-function App() {
-    return (
-        <div
-            style={{
-                minHeight: "100vh",
-                background: "#090614",
-                color: "white",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                fontFamily: "Arial",
-                fontSize: "30px",
-            }}
-        >
-            NEURO-EDU FUNCIONANDO
-        </div>
-    );
-}
+import App from "./App";
+import "./styles/neuro-edu.css";
 
 ReactDOM.createRoot(
     document.getElementById("root")!
