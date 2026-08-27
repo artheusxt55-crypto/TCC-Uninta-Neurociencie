4946c262aa601ae46af29d8f5ba44bd5d40db82e
Githubtheus <artheusxt55@gmail.com>
2026-08-26 21:47:39 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from "./App";
 import "./styles/neuro-edu.css";
 
 ReactDOM.createRoot(
-    document.getElementById("root")!
+    document.getElementById("root") as HTMLElement
 ).render(
     <React.StrictMode>
         <App />
