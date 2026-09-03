b4c8365f48e60bd26e5ef16c44f894cc38505a48
Githubtheus <artheusxt55@gmail.com>
2026-09-02 21:54:07 -0300

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,10 +1,12 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://kczzuvkuubeqdokjihrm.supabase.co";
+const SUPABASE_URL =
+  "https://kczzuvkuubeqdokjihrm.supabase.co";
 
-const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const SUPABASE_KEY =
+  "sb_publishable_h32y9_S9RBKvZ1sYqPSD5A_7UY94sho";
 
 export const supabase = createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
+  SUPABASE_URL,
+  SUPABASE_KEY
 );
