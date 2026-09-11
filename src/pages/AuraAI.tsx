e0d01d71eb590493c9568df076e01e35a8e89792
Githubtheus <artheusxt55@gmail.com>
2026-09-11 10:53:21 -0300

    Update import path for NeuralOrb component

--- src/pages/AuraAI.tsx
+++ src/pages/AuraAI.tsx
@@ -43,7 +43,7 @@ import {
   AlertTriangle,
 } from "lucide-react";
 
-import NeuralOrb, { type AuraState } from "./NeuralOrb.tsx";
+import NeuralOrb from "../components/NeuralOrb";
 import "./aura-educacube.css";
 
 /* ================================================================
